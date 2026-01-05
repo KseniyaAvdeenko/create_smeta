0a63422f0f57ac6d11ff564ec60b9a94ae8be5f5
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2026-01-05 15:04:27 +0300

    add app  actions

---
+++ client/src/store/actions/app.action.ts
@@ -0,0 +1,22 @@
+import {AppDispatch} from "../index";
+import {appReducer} from "../reducers/app.reducer";
+import {Theme} from "../../interface/IApp";
+
+
+
+export const getCurrentThemeMode = () => async (dispatch: AppDispatch) => {
+    const data = await window.electron.initial();
+    dispatch(appReducer.actions.loadCurrentThemeMode(data ? 'dark' : 'light'))
+}
+
+export const changeCurrentThemeMode = (theme: Theme) => async (dispatch: AppDispatch) => {
+    if (theme === 'dark') await window.electron.dark();
+    if (theme === 'light') await window.electron.light();
+    dispatch(appReducer.actions.changeCurrentThemeMode(theme));
+}
+
+export const getNetworkConnection = () => async (dispatch: AppDispatch) => {
+    const data = await window.electron.isOnline();
+    if (!data) dispatch(appReducer.actions.loadNetworkConnection(false));
+    dispatch(appReducer.actions.loadNetworkConnection(data));
+}
